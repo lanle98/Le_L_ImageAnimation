ce122f0e2afae0d5ebcc8fa8d3c17e8e132f3a36
Darren Le <darrenle98@gmail.com>
2019-01-28 13:37:32 -0500

    added array for images

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -1,21 +1,50 @@
 (() => {
 	console.log('fired');
 
-	const theButton = document.querySelectorAll("#buttonHolder img");
+	//set up the puzzle pieces and boards
+	//
+	// need a reference to each piece that we want to create
+	const thePieces = ["topLeft","topRight","bottomLeft","bottomRight"];
 
-	// window.addEventListener("click",changeHeadline);
-	// window.addEventListener("load",changeSubhead);
+	//get a reference to the drag side
+	let piecesBoard = document.querySelector(".puzzle-pieces");
+	let puzzleBoard = document.querySelector(".puzzle-board");
 
-	function changeHeadline()
+	// get a reference to the butons at the bottom so we can change the puzzle
+	let puzzleSelectors = document.querySelectorAll("#buttonHolder img");
+
+	//functions og in the middle
+	function createPuzzlePieces(pictureIndex)
 	{
-		document.querySelector("h1").textContent = "Hey There From JS!";
-		document.querySelector("p").textContent = "This is the Subhead";
+		//generate images here -> need to make 4 (top left, right, bottom left, right)
+		thePieces.forEach((piece,index) =>{
+			let newPuzzlePiece = `<img id="piece${index}" class="puzzle-image"
+			 src="images/${piece + pictureIndex}.jpg" alt="puzzle piece">`;
+
+			 piecesBoard.innerHTML += newPuzzlePiece;
+		});
+
+		// debugger;
 	}
-	// set up the puzzle pieces and boards
-	//
-	//
-	
-	theButton.forEach(button => button.addEventListener("mousemove",changeHeadline));
 
 
+	function resetPuzzlePieces()
+	{
+		// change the current puzzle, regenerate the pieces
+		//debugger;
+		//clean out the puzzle pieces div
+		piecesBoard.innerHTML = "";
+
+
+		//generate new pieces
+		createPuzzlePieces(this.dataset.puzzleref);
+	}
+
+	//event handling goes here
+	puzzleSelectors.forEach(button => button.addEventListener("click",resetPuzzlePieces));
+
+	// call this function to set up / generate the pieces on load
+	createPuzzlePieces(0);
+
+	debugger;
 })();
